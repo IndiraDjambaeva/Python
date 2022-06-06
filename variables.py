b069774bbe9812786c3955078ae451213cda8932
Indira Djambaeva <indira.djambaeva.kk@gmail.com>
2022-06-07 00:25:42 +0600

    Variables operations

--- variables.py
+++ variables.py
@@ -8,5 +8,21 @@
 
 #Multiple assignment
 x, y, name, is_cool = (1, 2.5, "Brad", True)
+print (x, y, name, is_cool) 
 
-print (x) 
+#Basic math
+a = x + y 
+print(a)
+
+#Check type
+print(type(is_cool))
+
+#Casting
+x=str(x)
+y=int(y)
+z=float(y)
+#Check type
+print(type(x))
+print(type(y))
+print(y)
+print(z)
